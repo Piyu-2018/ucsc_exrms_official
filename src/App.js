b42e529adee91f908adb67da2872a4f06604752f
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-04 22:58:42 +0530

    Create  new repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './Component/Navbar';
+import Home from './pages/Home';
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Home />}/>
+        <Route path='*' element={<Navigate to="/"/>}/>
+    
+      </Routes>
+
     </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './../style/home.css';
+import {Helmet} from 'react-helmet';
+// import 'ui-neumorphism/dist/index.css';
+// import { Button } from 'ui-neumorphism';
+
+function Home() {
+  return (
+    <div className='body'>
+        <Helmet>
+                <style>{'body { background-image: url("./pubImgs/background.png") !important; }'}</style>
+        </Helmet>
+        <div className='homeSection'>
+        <div className='leftSection'>
+          <h3>WE WILL HELP YOU SELECT THE BEST EDUCATION COURSE FOR YOU.</h3>
+          <p>After leaving school it is hard to find the correct path and career to pursue. We will help you find the most suitable higher educational options with our system.</p>
+          {/* <Button color='#ccc' bgColor='#06283D' rounded width='50px' className='registerBtn'>Register Now</Button> */}
+        </div>
+        <img src={require('./../assets/imgs/homeVector.png')} />
+        </div>
+    </div>
+  )
+}
+
+export default Home
