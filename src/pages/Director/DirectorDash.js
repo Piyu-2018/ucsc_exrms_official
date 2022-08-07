f7a3c27dd26da44045ff1b39c626e5e2e48424f9
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-07 21:50:50 +0530

    This commit for develop navigation and drawer using material ui

---
+++ src/pages/Director/DirectorDash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DirectorDrawer from '../../Component/DirectorDrawer';
+
+function DirectorDash() {
+  return (
+    <div>
+      <DirectorDrawer/>
+    </div>
+  )
+}
+
+export default DirectorDash
