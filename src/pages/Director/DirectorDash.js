cf64b1e6ced7f4c4ea5471d10931e7eab1c1178b
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-09 18:41:38 +0530

    This commit for developing director dashboard

--- src/pages/Director/DirectorDash.js
+++ src/pages/Director/DirectorDash.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import DirectorDrawer from '../../Component/DirectorDrawer';
+import {Helmet} from 'react-helmet';
+import DirectorDrawer from '../../Component/Director/DirectorDrawer';
 
 function DirectorDash() {
   return (
     <div>
+      <Helmet>
+        <style>{'body { background: #E4EBF5 !important; }'}</style>
+      </Helmet>
       <DirectorDrawer/>
     </div>
   )
