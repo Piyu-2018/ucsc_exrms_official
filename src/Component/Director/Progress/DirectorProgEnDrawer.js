83dd9d72cf53c4d2c60f7bcb36821efbaec3668e
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-10 18:26:24 +0530

    This commit for developing director progress page

--- src/Component/Director/Progress/DirectorProgEnDrawer.js
+++ src/Component/Director/Progress/DirectorProgEnDrawer.js
@@ -16,11 +16,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Navbar from '../Navbar';
-import DirectorUnderContent from './DirectorUnderContent';
-import DirectorUnderYear from './DirectorUnderyear';
-import DirectorUnderSeach from './DirectorUnderSeach';
-import DirectorUnderTable from './DirectorUnderTable';
+import Navbar from '../../Navbar';
+import DirectorProgEnContent from './DirectorProgEnContent';
+import DirectorProgEnBar from './DirectorProgEnBar';
+import DirectorProgEnTable from './DirectorProgEnTable';
 
 import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
@@ -184,10 +183,9 @@ export default function DirectorDash() {
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <DirectorUnderContent/>
-        <DirectorUnderYear/> 
-        <DirectorUnderSeach/>
-        <DirectorUnderTable/>
+        <DirectorProgEnContent/>
+        <DirectorProgEnBar/> <br></br>
+        <DirectorProgEnTable/>
       </Box>
 
     </Box>
