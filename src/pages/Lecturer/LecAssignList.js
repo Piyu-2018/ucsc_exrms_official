61c53dd250d3b53a4341145f7992ea1e2d37518a
DinilR <dinilsratnayake@gmail.com>
2022-08-11 23:10:29 +0530

    commit for adding a theme for typography

--- src/pages/Lecturer/LecAssignList.js
+++ src/pages/Lecturer/LecAssignList.js
@@ -1,33 +1,39 @@
-import { Box, Grid, Typography } from '@mui/material'
-import React from 'react'
-import LecAssignmentCourses from '../../Component/Lecturer/LecAssignmentCourses'
-import LecAssignTable from '../../Component/Lecturer/LecAssignTable'
-import LecNavBar from '../../Component/Lecturer/LecNavBar'
-import LecAddAssign from './LecAddAssign'
-import LecSidebar from './LecSidebar'
+import { Box, createTheme, Grid, Typography } from "@mui/material";
+import React from "react";
+import LecAssignmentCourses from "../../Component/Lecturer/LecAssignmentCourses";
+import LecAssignTable from "../../Component/Lecturer/LecAssignTable";
+import LecNavBar from "../../Component/Lecturer/LecNavBar";
+import LecAddAssign from "./LecAddAssign";
+import LecSidebar from "./LecSidebar";
+
+const theme = createTheme({
+  typography: {
+    h3: {
+      color: "#06283D",
+    },
+  },
+});
 
 function LecAssignList() {
   return (
     <>
-        <Box>
-            <LecNavBar />
-            <Grid container spacing={1} justifyContent="space-between">
-                <Grid item xs={4} sm={2}>
-                  <LecSidebar/>
-                </Grid>
-                <Grid item xs={8} sm={10}>
-                  <Typography variant="h3">
-                    Assignments (SCS3201)
-                  </Typography>
-                  <LecAssignTable/>
-                  <LecAddAssign/>
-
-                </Grid>    
-                
-            </Grid>
-        </Box>
+      <Box>
+        <LecNavBar />
+        <Grid container spacing={1} justifyContent="space-between">
+          <Grid item xs={4} sm={2}>
+            <LecSidebar />
+          </Grid>
+          <Grid item xs={8} sm={10}>
+            <Typography variant="h3" theme={theme}>
+              Assignments (SCS3201)
+            </Typography>
+            <LecAssignTable />
+            <LecAddAssign />
+          </Grid>
+        </Grid>
+      </Box>
     </>
-  )
+  );
 }
 
-export default LecAssignList
+export default LecAssignList;
