3d6ec8ee67185be54255c9ed1d8956fdc939cf78
DinilR <dinilsratnayake@gmail.com>
2022-08-15 15:14:20 +0530

    commit for sidebar button testing

--- src/pages/Lecturer/LecassignMarkTable.js
+++ src/pages/Lecturer/LecassignMarkTable.js
@@ -7,6 +7,16 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Button, createTheme, Typography } from "@mui/material";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
+
+const theme = createTheme({
+  typography: {
+    h3: {
+      color: "#06283D",
+    },
+  },
+});
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -28,16 +38,53 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(index, name, marks, more) {
+  return { index, name, marks, more };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData(
+    <Typography variant="h6" theme={theme}>
+      19001411
+    </Typography>,
+    <Typography variant="h6" theme={theme}>
+      B.G.D.S.B. Ratnayake
+    </Typography>,
+    <Typography variant="h6" theme={theme}>
+      90
+    </Typography>,
+    <Button variant="contained" endIcon={<DoubleArrowIcon />}>
+      Enter Marks
+    </Button>
+  ),
+  createData(
+    <Typography variant="h6" theme={theme}>
+      19001428
+    </Typography>,
+    <Typography variant="h6" theme={theme}>
+      J.D. Ratnayake
+    </Typography>,
+    <Typography variant="h6" theme={theme}>
+      80
+    </Typography>,
+    <Button variant="contained" endIcon={<DoubleArrowIcon />}>
+      Enter Marks
+    </Button>
+  ),
+  createData(
+    <Typography variant="h6" theme={theme}>
+      19001411
+    </Typography>,
+    <Typography variant="h6" theme={theme}>
+      B.G.D.S.B. Ratnayake
+    </Typography>,
+    <Typography variant="h6" theme={theme}>
+      -
+    </Typography>,
+    <Button variant="contained" endIcon={<DoubleArrowIcon />}>
+      Enter Marks
+    </Button>
+  ),
 ];
 
 function LecassignMarkTable() {
@@ -46,23 +93,21 @@ function LecassignMarkTable() {
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            <StyledTableCell>Index Number</StyledTableCell>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell>Marks</StyledTableCell>
+            <StyledTableCell>More Options</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.name}>
+            <StyledTableRow key={row.index}>
               <StyledTableCell component="th" scope="row">
-                {row.name}
+                {row.index}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+              <StyledTableCell>{row.name}</StyledTableCell>
+              <StyledTableCell>{row.marks}</StyledTableCell>
+              <StyledTableCell>{row.more}</StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
