cce7ec8ef337dfd09556bb3f3826a3ec5058a30d
DinilR <dinilsratnayake@gmail.com>
2022-08-17 03:29:55 +0530

    commit for initiating back end and login and register

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+//middleware
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+// routes
+const authRouter = require("./routes/authRoutes");
+// const settingsRouter = require("./routes/settingsRoutes")
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+
+// routes
+app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/settings", settingsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+//port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server is listening on port 5000");
+});
