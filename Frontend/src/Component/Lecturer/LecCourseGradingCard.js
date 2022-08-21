db4091dc3921be09ab0e952d8e0c2ae7bfd8f369
DinilR <dinilsratnayake@gmail.com>
2022-08-21 21:55:00 +0530

    commit for adding subject wise progress

---
+++ Frontend/src/Component/Lecturer/LecCourseGradingCard.js
@@ -0,0 +1,40 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+// import { Box } from '@mui/system';
+import React from "react";
+import { Link } from "react-router-dom";
+
+function LecCourseGradingCard(props) {
+  console.log(props);
+
+  return (
+    <>
+      <Card sx={{ maxWidth: "90%" }}>
+        <CardMedia
+          component="img"
+          height="100"
+          image="./pubImgs/computerscience.png"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.CourseCode}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.CourseName}
+          </Typography>
+          <Button sx={{ mt: 2 }} size="small">
+            <Link to={"/lec_course_results/"}>View Progress</Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
+
+export default LecCourseGradingCard;
