3d86be024809a90f22650f0df2ad008ea7413b0d
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-21 13:02:02 +0530

    This commit for developing Dashboard page

--- Frontend/src/pages/Director/DirectorSidebar.js
+++ Frontend/src/pages/Director/DirectorSidebar.js
@@ -51,13 +51,6 @@ export default function DirectorSidebar() {
         <ListItemText primary="Home" />
       </ListItemButton>
       <Divider />
-      <ListItemButton to='/directorDashboard'>
-        <ListItemIcon>
-          <DashboardRoundedIcon />
-        </ListItemIcon>
-        <ListItemText primary="Dashboard" />
-      </ListItemButton>
-      <Divider />
       <ListItemButton to='/directorUnder'>
         <ListItemIcon>
           <PeopleAltRoundedIcon/>
