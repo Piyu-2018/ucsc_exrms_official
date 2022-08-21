c583016f18b638e1d8d82d9d0b7cac8aef900bf0
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-21 17:20:02 +0530

    This commit to director examination

---
+++ Frontend/src/Component/Director/Examination/UnderDrawer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Category from './Category';
+import UnderSeach from './UnderSeach';
+import UnderTable from './UnderTable';
+import DirectorSidebar from '../DirectorSidebar';
+import { Grid} from "@mui/material";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+function UnderDrawer() {
+  const open = true;
+  console.log(open);
+
+  return (
+    <>
+     
+
+        <Grid container spacing={1} justifyContent="space-between" paddingTop="10px" paddingRight = "40px">
+          <Grid item sm={4} md={2}>
+            <DirectorSidebar open={open} />
+          </Grid>
+          <Grid item sm={8} md={10} >
+          <h2><AssignmentIcon sx={{ fontSize: 20 }}/> Examination</h2>
+            <Category/> <br></br>
+
+            <UnderSeach/><br></br>
+            <UnderTable/>
+          </Grid>  
+        </Grid>
+    </>
+  );
+}
+
+export default UnderDrawer;
+
---
+++ Frontend/src/Component/Director/Examination/UnderSeach.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import UnderOp1 from './UnderOp1';
+import UnderOp2 from './UnderOp2';
+import UnderOp3 from './UnderOp3';
+import UnderOp4 from './UnderOp4';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function AutoGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+
+
+            <Grid item xs={3} md={12} container spacing={4} columnpacing={10} >
+                <Grid item xs={12} md={3}>
+                    <UnderOp1/>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <UnderOp2/>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <UnderOp3/>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <UnderOp4/>
+                </Grid>
+            </Grid>
+
+
+    </Box>
+  );
+}
