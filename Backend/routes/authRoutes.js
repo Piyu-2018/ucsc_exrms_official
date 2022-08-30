b3a99a2524c1d9ef2a8ce75336ac4e2bd739f12e
DinilR <dinilsratnayake@gmail.com>
2022-08-30 23:06:28 +0530

    commif for adding password recovery functions in backend

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -3,11 +3,12 @@ const express = require("express");
 const { validateToken } = require("../middleware/AuthMiddleware");
 
 // import controllers
-const { login, register } = require("../controllers/authController");
+const { login, register, emailCheck } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/emailcheck", emailCheck);
 
 module.exports = router;
