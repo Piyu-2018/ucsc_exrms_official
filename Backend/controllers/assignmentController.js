d859f5b6e8a6e021a5ee0930d785ab966194ad90
SSWidanapathirana <84173987+SSWidanapathirana@users.noreply.github.com>
2022-09-09 16:13:37 +0530

    committ 2

--- Backend/controllers/assignmentController.js
+++ Backend/controllers/assignmentController.js
@@ -85,7 +85,10 @@ const getUndergraduates = asyncHandler(async (req, res) => {
 
 const getResult = asyncHandler(async (req, res) => {
   const mark_id = (req.params.id1);
+  const year = (req.params.id2);
+  const semester = (req.params.id2);
   const degree = (req.params.id2);
+  const course_name = (req.params.id2);
   
   const mark = [];
 
