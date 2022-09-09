08593cf88873bb99178f6aeb160a6ad0a24067af
SSWidanapathirana <84173987+SSWidanapathirana@users.noreply.github.com>
2022-09-09 16:03:42 +0530

    commit

--- Backend/controllers/assignmentController.js
+++ Backend/controllers/assignmentController.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-const { user, course, lecturer_courses, assignments, student } =
+const { user, course, lecturer_courses, assignments, student, mark } =
   new PrismaClient();
 
 const getCourses = asyncHandler(async (req, res) => {
@@ -84,16 +84,16 @@ const getUndergraduates = asyncHandler(async (req, res) => {
 });
 
 const getResult = asyncHandler(async (req, res) => {
-  const ac_year_ID = (req.params.id1);
-  const degree_type = (req.params.id2);
+  const mark_id = (req.params.id1);
+  const degree = (req.params.id2);
   
-  const student = [];
+  const mark = [];
 
   connection.query(
-    "SELECT student.*,user.* FROM user,student WHERE user.user_id = student.user_id AND student.ac_year_ID =" +'"'+
-      ac_year_ID +'"'+
+    "SELECT student.*,user.* FROM user,student WHERE user.user_id = student.user_id AND student.mark_id  =" +'"'+
+    mark_id  +'"'+
       " AND student.degree_type =" +
-      '"'+degree_type+'"',
+      '"'+degree+'"',
     function (error, results, fields) {
       if (error) throw error;
 
@@ -242,15 +242,14 @@ const getAssignMarks = asyncHandler(async (req, res) => {
   // })
 });
 
-<<<<<<< HEAD
-module.exports = { getCourses, getAssign, getUndergraduates,getResult, assignAdd,assignMarkAdd };
-=======
+
 module.exports = {
   getCourses,
   getAssign,
   getUndergraduates,
+  getResult,
   assignAdd,
   assignMarkAdd,
   getAssignMarks,
 };
->>>>>>> 5129cad3e23c02101ecbfdb96c10eed1768fceed
+
