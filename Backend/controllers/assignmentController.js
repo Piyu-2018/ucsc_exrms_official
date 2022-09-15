8423bf5f771d1e5e54ef4c55915f92253dc9af17
DinilR <dinilsratnayake@gmail.com>
2022-09-16 00:53:01 +0530

    commit for adding forgot password page

--- Backend/controllers/assignmentController.js
+++ Backend/controllers/assignmentController.js
@@ -5,14 +5,22 @@ const { StatusCodes } = require("http-status-codes");
 // const { user, course, lecturer_courses, assignments } = new prismaClient();
 
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//   host: "sql238.main-hosting.eu",
+//   user: "u117929562_ucscExrmsUser",
+//   password: "lT:@>w0y4",
+//   database: "u117929562_ucscEXRMS",
+// });
+
+// connection.connect();
+
+var connection = mysql.createPool({
+  connectionLimit : 10,
   host: "sql238.main-hosting.eu",
   user: "u117929562_ucscExrmsUser",
   password: "lT:@>w0y4",
   database: "u117929562_ucscEXRMS",
 });
-
-connection.connect();
 
 // const { user, course, lecturer_courses, assignments, student, mark } =
 //   new PrismaClient();
