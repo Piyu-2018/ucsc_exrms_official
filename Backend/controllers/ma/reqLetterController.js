b608b3013a01a6e0c6055357388576b78fdcc84f
DinilR <dinilsratnayake@gmail.com>
2022-10-22 21:04:30 +0530

    commit for defining another function

--- Backend/controllers/ma/reqLetterController.js
+++ Backend/controllers/ma/reqLetterController.js
@@ -5,14 +5,14 @@ const { StatusCodes } = require("http-status-codes");
 // const { user, course, lecturer_courses, assignments } = new prismaClient();
 
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "u117929562_ucscexrms",
-});
 
-connection.connect();
+var connection = mysql.createPool({
+  connectionLimit: 10,
+  host: "sql238.main-hosting.eu",
+  user: "u117929562_ucscExrmsUser",
+  password: "lT:@>w0y4",
+  database: "u117929562_ucscEXRMS",
+});
 
 const { user, course, lecturer_courses, assignments, student } =
   new PrismaClient();
